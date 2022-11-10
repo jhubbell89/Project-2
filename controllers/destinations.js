8924e82e3080be6b8db1eb3c90ca07cfd301009f
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-11-10 14:25:08 -0800

    get destinations submit to work correctly

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,28 +1,30 @@
 
 const Destination = require('../models/destination');
-// const Trip = require('../models/trip')
+const Trip = require('../models/trip')
 
 module.exports = {
   new: newDestination,  
   create, 
-//   addToLocations
+  addToPlace,
 }
-
-// function addToLocations(req, res) {
-//     Trip.findById(req.params.id, function (err, trip))
-// }
 
 function newDestination(req, res) {
   Destination.find(function(err, destination) {
     res.render('destinations/index', {destination})
-    }
-    )
+  })
 }
-
-
 
 function create(req, res){
   var destination = new Destination(req.body)
   destination.save(function(err){destination})
-          res.redirect('index')
+  res.redirect('index')
+}
+
+function addToPlace(req, res) {
+  Trip.findById(req.params.id, function(err, trip) {
+    trip.place.push(req.body.destinationId);
+    trip.save(function(err) {
+      res.redirect(`/trips/${trip._id}`);
+    })
+  })
 }
